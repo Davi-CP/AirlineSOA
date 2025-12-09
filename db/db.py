6a8177b29ae38595b191a56823a755c0266e63cc
Davi-CP <dvcp.costa@gmail.com>
2025-12-09 20:03:39 -0300

    Correção do db.py para criar tabela voos também

--- db/db.py
+++ db/db.py
@@ -9,6 +9,11 @@ conexao = s.connect(str(DB_PATH))
 #criar cursor
 cursor = conexao.cursor()
 
+
+#criar tabela voos
+sql = "CREATE TABLE IF NOT EXISTS voos (id INTEGER PRIMARY KEY, numero_voo INTEGER, origem TEXT, destino TEXT, data_voo TEXT, horario_partida TEXT, horario_chegada TEXT, capacidade INTEGER, criado_em TEXT)"
+cursor.execute(sql)
+
 #criar tabela reservas
 sql = "CREATE TABLE IF NOT EXISTS reservas (id INTEGER PRIMARY KEY, numero_voo INTEGER, cpf INTEGER, data_reserva TEXT, status TEXT, criado_em TEXT,FOREIGN KEY (numero_voo) REFERENCES voos (id))"
 cursor.execute(sql)
